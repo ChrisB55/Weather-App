6047086b57ad9891ebe0917fdfbbfca1cabad0e1
Christopher W. Brown <christopherwilliambrown@gmail.com>
2016-12-05 12:28:17 -0500

    Start app.js file
    update angular script src
    add ng-view
    start building controllers in app.js file

---
+++ app.js
@@ -0,0 +1,10 @@
+var weatherApp = angular.module('weatherApp');
+
+
+weatherApp.controller('landingController', ['$scope', function($scope) {
+
+}]);
+
+weatherApp.controller('resultController', ['$scope', function($scope) {
+
+}]);
