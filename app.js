2e903da6abb729b17ac6a991f32e82f6df110092
Christopher W. Brown <christopherwilliambrown@gmail.com>
2016-12-05 14:44:40 -0500

    Add code re controller build out

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-var weatherApp = angular.module('weatherApp');
+var weatherApp = angular.module('weatherApp', ['ngroute', 'ngResourse']);
+
+weatherApp.config(function($routeProvider) {
+
+  $routeProvider
+
+    .when ('/', {
+      templateUrl:'project_htm/landing.htm',
+      controller :'landingController'
+      })
+
+    .when ('/', {
+      templateUrl:'project_htm/landing.htm',
+      controller :'resultController'
+    })
+
+});
+
 
 
 weatherApp.controller('landingController', ['$scope', function($scope) {
