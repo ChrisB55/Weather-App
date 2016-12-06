d26f02e22a50280c577442fdd88e56fa519f41c8
Christopher W. Brown <christopherwilliambrown@gmail.com>
2016-12-06 11:41:19 -0500

    Add new index, js files
    Replace template with simple index page.
    Add /angular js files to allow for SPA build.

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-var weatherApp = angular.module('weatherApp', ['ngroute', 'ngResourse']);
-
-weatherApp.config(function($routeProvider) {
-
-  $routeProvider
-
-    .when ('/', {
-      templateUrl:'project_htm/landing.htm',
-      controller :'landingController'
-      })
-
-    .when ('/', {
-      templateUrl:'project_htm/landing.htm',
-      controller :'resultController'
-    })
-
-});
-
-
-
-weatherApp.controller('landingController', ['$scope', function($scope) {
-
-}]);
-
-weatherApp.controller('resultController', ['$scope', function($scope) {
-
-}]);
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
